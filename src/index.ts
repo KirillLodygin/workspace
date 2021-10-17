24a1318d4eeb39467aea05fa104a1a5289a82865
Kirilll <vilyam13@gmail.com>
2021-10-17 22:53:07 +0300

    create template & templateEngine

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { MainPage } from '~/pages/MainPage/MainPage';
+import { TownSuggest } from '~/components/TownSuggest/TownSuggest';
+
 import './index.sass';
 
 const app = document.querySelector<HTMLDivElement>('.app');
+
+const mainPage = new MainPage(app);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const townSuggest = new TownSuggest(mainPage.element);
